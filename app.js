ba245de4d31049d54b42526af8d888c744e1ad31
bhite1 <bhite1@users.noreply.github.com>
2023-08-15 08:35:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,25 +13,47 @@ fs.readFile('question-bank.json', 'utf8', (err, data) => {
 });
 
 let currentQuestionIndex = 0;
+let userScore = 0;
 
 app.get('/', (req, res) => {
-    res.render('quiz', { question: questions_data[currentQuestionIndex], message: '', explanation: '' });
+    res.render('quiz', {
+        question: questions_data[currentQuestionIndex],
+        questionNumber: currentQuestionIndex + 1,
+        totalQuestions: questions_data.length,
+        userScore: userScore,
+        selected_answer: "",
+        message: '',
+        explanation: ''
+    });
 });
 
 app.post('/', (req, res) => {
     const selected_answer = req.body.answer;
     const correct_answer = questions_data[currentQuestionIndex].answer;
-    
-    let message = selected_answer === correct_answer ? "Correct!" : "Incorrect!";
-    let explanation = questions_data[currentQuestionIndex].explanation;
-    
+
+    let message = '';
+    if (req.body.submit === "Submit Answer") {
+        message = selected_answer === correct_answer ? "Correct!" : "Incorrect!";
+        if (selected_answer === correct_answer) userScore += 1;
+    }
+
+    let explanation = req.body.submit === "Submit Answer" ? questions_data[currentQuestionIndex].explanation : '';
+
     if (req.body.submit === "Next") {
         currentQuestionIndex = (currentQuestionIndex + 1) % questions_data.length;
         message = '';
         explanation = '';
     }
-    
-    res.render('quiz', { question: questions_data[currentQuestionIndex], message: message, explanation: explanation });
+
+    res.render('quiz', {
+        question: questions_data[currentQuestionIndex],
+        questionNumber: currentQuestionIndex + 1,
+        totalQuestions: questions_data.length,
+        userScore: userScore,
+        selected_answer: selected_answer,
+        message: message,
+        explanation: explanation
+    });
 });
 
 const PORT = 3000;
